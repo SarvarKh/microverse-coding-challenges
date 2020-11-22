89048fe43f988143200244473e72abfab2615ba8
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-11-22 15:07:19 +0500

    Pseudo code was added

--- Microverse coding challenges/drawing-book.js
+++ Microverse coding challenges/drawing-book.js
@@ -1,6 +1,15 @@
-function pageCount(n, p) {
-  /*
-   * Write your code here.
-   */
 
+// Pseudo code | Strategy
+
+// 1. Make an array of pages;
+// 2. Count number of pages from the front to arrive to the page;
+// 3. Count number of pages from back to arrive to the page;
+// 4. If countFront > countBack => return countBack;
+//  else return countFront;
+
+
+function pageCount(n, p) {
+  console.log("Hello");
 }
+
+pageCount(6, 2);
