0c8fe4cecf339e7c3dc0c650f0c8d41ad26b4631
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-16 20:58:25 +0500

    Finish check for cost against given budget

--- bin/electronics-shop.rb
+++ bin/electronics-shop.rb
@@ -13,16 +13,24 @@ def getMoneySpent(keyboards, drives, b)
   drives_sorted = drives.sort_by { |x| -x }
   budget = b[0]
 
+  return -1 if keyboards_sorted[keyboards_sorted.size - 1] + drives_sorted[drives_sorted.size - 1] > budget
+
   i = 0
   while i < keyboards_sorted.size
     y = 0
     while y < drives_sorted.size
-      
-      # p "i: #{i}, y: #{y}"
+      current_sum = keyboards_sorted[i] + drives_sorted[y]
+      alternative_sum = keyboards_sorted[i + 1] + drives_sorted[y - 1]
+      # p "i: #{i}, y: #{y}, current_sum: #{current_sum}, alternative_sum: #{alternative_sum}"
+      if current_sum <= budget && current_sum >= alternative_sum
+        return current_sum
+      elsif current_sum <= budget && current_sum < alternative_sum
+        return alternative_sum
+      end
       y += 1
     end
     i += 1
   end
 end
 
-getMoneySpent([3, 1], [5, 2, 8], [10, 2, 3])
+puts getMoneySpent([3, 1], [5, 2, 8], [10, 2, 3])
