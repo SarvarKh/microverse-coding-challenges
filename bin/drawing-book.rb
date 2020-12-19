f7aa2f79b504d817ce6650e977e02316a06a970f
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-19 23:23:02 +0500

    Solve problem

--- bin/drawing-book.rb
+++ bin/drawing-book.rb
@@ -3,3 +3,16 @@
 # 2. Count back moves (b) 
 # 3. Devide by 2 and round to integer
 # 4. Compare and return lowest one 
+
+def pageCount(n, p)  
+  f = (p + 1) / 2
+  b = (n - p) / 2
+
+  if (f > b)
+    b
+  else
+    f
+  end
+end
+
+puts pageCount(6, 2)
